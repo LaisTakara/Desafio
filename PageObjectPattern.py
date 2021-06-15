f59a3d1453ca4fa0d65d22d9bdacd071f64135c4
Laís Floriano <laisflorianor@gmail.com>
2021-06-15 20:23:42 -0300

    PO

--- PageObjectPattern.py
+++ PageObjectPattern.py
@@ -1,33 +1,32 @@
+from selenium import webdriver
+from selenium.webdriver import Firefox
+from abc import ABC
 from selenium.webdriver.common.by import By
 
-class Login:
-   def __init__(self, webdriver):
-    self.UserName = (By.ID, 'userName')
-    self.Password = (By.ID, 'password')
-    self.BotaoLogin = (By.ID, 'login')
-    self.BotaoNewUser = (By.ID, 'newUser')
-       
-    self.webdriver = webdriver
+class PageElement(ABC):
+    def __init__(self, webdriver):
+        self.webdriver = webdriver
 
-    def create_Login(self, UserName, Password, BotaoLogin, BotaoNewUser):
-        self.webdriver.find_element(*self.UserName).send_keys(UserName)
-        self.webdriver.find_element(*self.Password).send_keys(Password)
-        self.webdriver.find_element(*self.BotaoLogin).click()
-    
-    ##self.webdriver.findelement(*self.BotaoNewUser).send_keys(BotaoLogin)
+class Login(PageElement):
+       UserName = (By.ID, 'userName')
+       Password = (By.ID, 'password')
+       BotaoLogin = (By.ID, 'login')
+       ##BotaoNewUser = (By.ID, 'newUser')
 
+       def create_Login(self, UserName, Password):
+           self.webdriver.find_element(*self.UserName).send_keys(UserName)
+           self.webdriver.find_element(*self.Password).send_keys(Password)
+           self.webdriver.find_element(*self.BotaoLogin).click()
 
-    from selenium.webdriver import Firefox 
+webdriver = Firefox()
 
-    webdriver = Firefox()
+url = 'https://demoqa.com/login'
 
-    url = 'https://demoqa.com/login'
+webdriver.get(url)
 
-    webdriver.get(url)
+login_page = Login (webdriver)
 
-    login_page = Login (webdriver)
-
-    login_page.create_Login(
-        'LaisFloriano',
-        '#Lais1022'
-    )
+login_page.create_Login(
+    UserName='LaisFloriano',
+    Password='#Lais1022'
+)
